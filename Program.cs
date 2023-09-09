589feb3fae42aecfd9e0c6b2f678bf59937b44fd
Aysegul ESKI <ayseguleskk@gmail.com>
2023-09-09 12:54:24 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazartesi);
+        Console.WriteLine((int)Gunler.Cumartesi);//index numarasini getirir
+
+        int sicaklik=27;
+
+        if(sicaklik <= (int)HavaDurumu.Normal)
+            Console.WriteLine("Disariya cikmak icin biraz daha havanin isinmasini bekleyelim.");
+        else if(sicaklik >= (int)HavaDurumu.Sicak)
+            Console.WriteLine("Disariya cikmak icin cok sicak bir gun.");
+        else if(sicaklik >= (int)HavaDurumu.Normal && sicaklik <(int)HavaDurumu.CokSicak)
+            Console.WriteLine("Hadi disariya cikalim!");       
+    }
+}
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+};
+
+enum HavaDurumu
+{
+    Soguk = 5,
+
+    Normal = 20,
+    Sicak = 25,
+    CokSicak = 30
+}
